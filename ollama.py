b12387a70e0beede25b6d174860e4efcfc28cc6d
jonhyuk0922 <jonhyuk0922@naver.com>
2024-10-16 12:09:24 +0000

    update ollama

---
+++ ollama.py
@@ -0,0 +1,9 @@
+import ollama
+print(ollama.__version__)
+# response = ollama.chat(modsel='mistral', message=[
+#     {
+#         'role': 'user',
+#         'content': 'Why is the sky blue?',
+#     },
+# ])
+# print(response['message']['content'])
